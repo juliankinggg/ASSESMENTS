c91de835a250da591c18d53bf73e350c3d2171d1
polsmpd@gmail.com <polsmpd@gmail.com>
2025-05-01 12:19:18 +1200

    Created a code for listing the activities and a code for entering your chosen activity

--- New folder/program.py
+++ New folder/program.py
@@ -17,4 +17,21 @@ while True:
     except ValueError:
         print("Please enter a valid input")
 
+camp_activities = ["Cultural Immersion, (5 days, Easy, $800 fee)", "Kayaking and Pancakes, (3 days, Moderate, $400 fee)", "Mountain Biking, (4 days, Difficult, $900 fee)"]
 
+print("These are the activities that our camp offers:")
+print(F"1. {camp_activities[0]}")
+print(F"2. {camp_activities[1]}")
+print(F"3. {camp_activities[2]}")
+
+while True:
+    try:
+        activity_chosen = int(input("Please enter either 1, 2 or 3 for your chosen activity: "))
+        if activity_chosen in [1, 2, 3]:
+            break
+        else:
+            print("Please enter either 1, 2 or 3 and pick an activity.")
+    except ValueError:
+        print("Invalid input, please put a number.")
+
+        
