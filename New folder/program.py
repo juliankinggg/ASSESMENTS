3bb1b7a82c1fe239c2cc04ea3f0421ae77aaba86
polsmpd@gmail.com <polsmpd@gmail.com>
2025-05-01 12:01:13 +1200

    created a code that asks for the users name and age

--- New folder/program.py
+++ New folder/program.py
@@ -1,2 +1,20 @@
-print("hello")
-print("Hi")
+#asks user for first name
+while True:
+    first_name = input("Please enter your first name: ")
+    if len(first_name) > 0:
+        break
+    else:
+        print("Please enter a valid input")
+
+#asks the user for their age 
+while True:
+    try:
+        age = int(input("Please enter your age: "))
+        if age > 4 and age < 18 :
+            break
+        else:
+            print("You must be 5 - 17 Years old to attend")
+    except ValueError:
+        print("Please enter a valid input")
+
+
